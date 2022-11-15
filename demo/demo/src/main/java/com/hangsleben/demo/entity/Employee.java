639b8e04b4a3eabd75253e811f2fed48f9d21935
Sarah-Hangsleben <114109326+Sarah-Hangsleben@users.noreply.github.com>
2022-11-15 08:47:25 -0600

    Updated DynamoDB table information

--- demo/demo/src/main/java/com/hangsleben/demo/entity/Employee.java
+++ demo/demo/src/main/java/com/hangsleben/demo/entity/Employee.java
@@ -2,37 +2,27 @@ package com.hangsleben.demo.entity;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@DynamoDBTable(tableName = "employee")
+@DynamoDBTable(tableName = "EmployeeTable")
 public class Employee {
 
-    @DynamoDBHashKey(attributeName = "employeeId")
     public String employeeId;
-    
-    @DynamoDBAttribute(attributeName = "firstName")
-    public String firstName;
-    
-    @DynamoDBAttribute(attributeName = "lastName")
-    public String lastName;
-    
-    @DynamoDBAttribute(attributeName = "email")
-    public String email;
-    
-    @DynamoDBAttribute(attributeName = "address")
-    public String address;
-    
-    @DynamoDBAttribute(attributeName = "department")
     public String department;
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String address;
+   
     
-    
+    @DynamoDBHashKey(attributeName = "employeeId")
     public String getEmployeeId() {
     	return employeeId;
     }
@@ -40,8 +30,17 @@ public class Employee {
     public void setEmployeeId(String employeeId) {
     	this.employeeId= employeeId;
     }
+    
+    @DynamoDBRangeKey(attributeName = "department")
+    public String getDepartment() {
+    	return department;
+    }
+    
+    public void setDepartment(String department) {
+    	this.department= department;
+    }
 
- 
+    @DynamoDBAttribute(attributeName = "firstName")
     public String getFirstName() {
     	return firstName;
     }
@@ -49,7 +48,8 @@ public class Employee {
     public void setFirstName(String firstName) {
     	this.firstName= firstName;
     }
-
+ 
+    @DynamoDBAttribute(attributeName = "lastName")
     public String getLastName() {
     	return lastName;
     }
@@ -57,7 +57,8 @@ public class Employee {
     public void setLastName(String lastName ) {
     	this.lastName= lastName;
     }
-     
+    
+    @DynamoDBAttribute(attributeName = "email")
     public String getEmail() {
     	return email;
     }
@@ -66,6 +67,7 @@ public class Employee {
     	this.email= email;
     }
     
+    @DynamoDBAttribute(attributeName = "address")
     public String getAddress() {
     	return address;
     }
@@ -74,11 +76,4 @@ public class Employee {
     	this.address= address;
     }
     
-    public String getDepartment() {
-    	return department;
-    }
-    
-    public void setDepartment(String department) {
-    	this.department= department;
-    }
 }
