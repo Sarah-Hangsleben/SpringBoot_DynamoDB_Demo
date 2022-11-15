55e2b2271fd44f5beea70fc5cc2042d41a62d039
Sarah-Hangsleben <114109326+Sarah-Hangsleben@users.noreply.github.com>
2022-11-15 08:49:13 -0600

    Added query endpoint

--- demo/demo/src/main/java/com/hangsleben/demo/repository/EmployeeRepository.java
+++ demo/demo/src/main/java/com/hangsleben/demo/repository/EmployeeRepository.java
@@ -1,9 +1,18 @@
 package com.hangsleben.demo.repository;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
 import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedScanList;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
 import com.hangsleben.demo.entity.Employee;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -28,21 +37,40 @@ public class EmployeeRepository {
         PaginatedScanList <Employee> paginatedList = dynamoDBMapper.scan(Employee.class, paginatedScanListExpression);
        return paginatedList;
      }
-    
-    public PaginatedScanList <Employee> getEmployeesByDepartment(Employee employee, String department){
-       DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
-       Map<String, Condition> filter = new HashMap<String, Condition>();
-       filter.put("department", new Condition().withComparisonOperator(ComparisonOperator.CONTAINS)
-                    .withAttributeValueList(new AttributeValue().withS(department)));
 
-       scanExpression.setScanFilter(filter);
-       PaginatedScanList<Employee> scanResult = dynamoDBMapper.scan(Employee.class, scanExpression);
-       return scanResult;
+    public PaginatedScanList <Employee> getEmployeesByDepartmentScan(String department){
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
+        Map<String, Condition> filter = new HashMap<String, Condition>();
+        filter.put("department", new Condition().withComparisonOperator(ComparisonOperator.CONTAINS)
+                        .withAttributeValueList(new AttributeValue().withS(department)));
+
+        scanExpression.setScanFilter(filter);
+        PaginatedScanList<Employee> scanResult = dynamoDBMapper.scan(Employee.class, scanExpression);
+        return scanResult;
     }
+    
+    public PaginatedQueryList < Employee > getEmployeesByDepartmentQuery(String employeeId, String department) {
+        Employee employee = new Employee();
+        employee.setEmployeeId(employeeId);
 
+        Condition condition = new Condition();
+        condition.withComparisonOperator(ComparisonOperator.EQ)
+            .withAttributeValueList(new AttributeValue().withS(department));
+
+        DynamoDBQueryExpression < Employee > queryExpression =
+            new DynamoDBQueryExpression < Employee > ()
+            .withHashKeyValues(employee)
+            .withRangeKeyCondition("department", condition)
+            .withLimit(10);
+
+        PaginatedQueryList < Employee > queryResult = dynamoDBMapper.query(Employee.class, queryExpression);
+
+        return queryResult;
+    }
+    
     public String deleteEmployeeById(String customerId) {
         dynamoDBMapper.delete(dynamoDBMapper.load(Employee.class, customerId));
         return "Customer Id : "+ customerId +" deleted";
     }
-
+    
 }
